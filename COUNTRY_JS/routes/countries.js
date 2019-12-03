6791d5c6217b02fde9b9de85d65897d4ab0c7644
corradomarco <corrado.marco@iisgalvanimi.edu.it>
2019-12-03 11:49:50 +0000

    finito

--- COUNTRY_JS/routes/countries.js
+++ COUNTRY_JS/routes/countries.js
@@ -3,11 +3,9 @@ var express = require('express'); //Carica il modulo express
 var router = express.Router(); //Ottiene un oggetto router
 var country = require('countryjs');
 
-nd(country.info('IT'));  //Restituisce le informazioni sull’italia
-});
 router.get('/info/:state/', function(req, res, next){ //Rendiamo lo stato un parametro
- res.send(req.params);
+ res.send(country.info(req.params.state));
 })
 
-module.exports = router; //esporta il modulo per poterlo usare in app.js
+module.exports = router;
 
